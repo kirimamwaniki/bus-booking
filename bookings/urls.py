2670e5873eaaa8777f376b6c7ad5205a2cce4fb2
unknown <kirimagriezman@gmail.com>
2025-09-20 12:05:47 +0300

    initial commit

--- bookings/urls.py
+++ bookings/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.main, name='main'),
     path('pay/<int:id>', views.pay, name='pay'),
     path('success/<int:id>', views.success, name='success'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
